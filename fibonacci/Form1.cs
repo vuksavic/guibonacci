9fd8f348d1e7328e4218f7d405af022506e67981
Vuk Savić <vuk@teknik.io>
2016-08-31 13:35:02 +0200

    somewhat complete rewrote of code
    rewrote large part of code to improve calculation time using System.Collections, System.Diagnostics's stopwatch
    added label that shows calculation time
    added error message (in case of errors)

--- fibonacci/Form1.cs
+++ fibonacci/Form1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Numerics;
 using System.Windows.Forms;
 
@@ -6,32 +8,84 @@ namespace fibonacci
 {
     public partial class Form1 : Form
     {
+        private Stopwatch sw = new Stopwatch();
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        public static BigInteger Fibonacci(long n)
+        // old calculation code left to be admired
+        //public static BigInteger Fibonacci(long n)
+        //{
+
+        //    BigInteger a = 0;
+        //    BigInteger b = 1;
+
+        //    for (long i = 0; i < n; i++)
+        //    {
+        //        BigInteger temp = a;
+        //        a = b;
+        //        b = temp + b;
+        //    }
+
+        //    return a;
+        //}
+
+        public static List<BigInteger> FibonacciList(int n)
         {
+            List<BigInteger> list = new List<BigInteger>((int)n);
+
             BigInteger a = 0;
+            if (n >= 0)
+                list.Add(a);
+
             BigInteger b = 1;
-            
-            for(long i = 0; i < n; i++)
+            if (n >= 1)
+                list.Add(b);
+
+            for (int i = 0; i < n; i++)
             {
                 BigInteger temp = a;
                 a = b;
                 b = temp + b;
+
+                list.Add(b);
             }
 
-            return a;
+            return list;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            listBox1.Items.Clear();
+            sw.Reset();
+            label1.Text = "";
 
-            for (int i = 0; i < Convert.ToInt64(textBox1.Text); i++)
-                listBox1.Items.Add(Fibonacci(i));
+            try
+            {
+                listBox1.Items.Clear();
+
+                int uLimit = int.Parse(textBox1.Text);
+
+                sw.Start();
+
+                List<BigInteger> list = FibonacciList(uLimit);
+
+                sw.Stop();
+
+                foreach (BigInteger f in list) listBox1.Items.Add(f);
+
+                label1.Text = "calculation time: " + sw.Elapsed.TotalMilliseconds + "ms";
+            }
+
+            catch
+            {
+
+                label1.Text = "error";
+
+                MessageBox.Show("error", "please enter how many fibonacci numbers you want to be calculated");
+            }
+
         }
     }
 }
